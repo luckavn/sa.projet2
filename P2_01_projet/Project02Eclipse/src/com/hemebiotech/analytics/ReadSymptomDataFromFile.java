b0ed30821340de66a131ef638adb3ff66c2e91cd
lucka <lucas.vannier@axa.fr>
2020-02-17 16:12:47 +0100

    Clean class ReadSymptomDataFromFile and method getSymptoms

--- P2_01_projet/Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ P2_01_projet/Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -4,33 +4,31 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
-
 	private String filepath;
-	
+
 	/**
-	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	@Override
+	/**
+	 * @return A List of String with Symptoms in it
+	 */
 	public List<String> getSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
@@ -39,9 +37,9 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			Collections.sort(result);
 		}
-		
 		return result;
-	}
 
+	}
 }
