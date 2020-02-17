919cdfe4a399f93bb1af269e16261bab6544f9b4
lucka <lucas.vannier@axa.fr>
2020-02-17 16:15:15 +0100

    Initialise Class CountSymptomDataFromList and his method countSymptom

---
+++ P2_01_projet/Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromList.java
@@ -0,0 +1,35 @@
+package com.hemebiotech.analytics;
+
+import java.util.*;
+
+/**
+ * Simple brute force implementation
+ *
+ */
+public class CountSymptomDataFromList implements ISymptomCounter {
+
+	@Override
+	/**
+	 * @param symptoms which is a simple list of symptoms
+	 * @return A map (key/value) of symptoms with occurences
+	 */
+	public Map<String, Integer> countSymptom(List<String> symptoms) {
+		Map<String, Integer> map = new TreeMap<>();
+
+		if (symptoms != null && !symptoms.isEmpty()) {
+			Collections.sort(symptoms);
+
+			for (String symptom : symptoms) {
+				if (map.containsKey(symptom)) {
+					map.put(symptom, map.get(symptom) + 1);
+				} else {
+					map.put(symptom, 1);
+				}
+			}
+		}
+		return map;
+	}
+}
+
+
+
