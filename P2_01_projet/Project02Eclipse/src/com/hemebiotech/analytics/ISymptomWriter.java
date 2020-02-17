5b45c3924b015f010d82d3131239718e22bd58c7
lucka <lucas.vannier@axa.fr>
2020-02-17 16:09:03 +0100

    Initialise IsymptomWriter interface for method signature writeSymptoms

---
+++ P2_01_projet/Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Anything that will write symptom data from an ordered map to a file (inside the project)
+ */
+public interface ISymptomWriter {
+	/**
+	 * @param mapOfSymptoms a map listing of all Symptoms and their occurences with no duplications
+	 * @return a void (nothing to use, but not null)
+	 */
+	void writeSymptoms(Map<String, Integer> mapOfSymptoms) throws IOException;
+}
+
+
